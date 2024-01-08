e1ee8e042e4433850f36e723a5e1eb3f4699a857
Sai Kiran <saikiran.hss@gmail.com>
2024-01-08 08:04:30 +0530

    Infrastructure Project
    First Commit

---
+++ RegisterationForm.Infrastructure/IUnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿
+using RegisterationForm.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegisterationForm.Infrastructure.IUnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<User> UserRepository { get; }
+        IGenericRepository<Skill> SkillRepository { get; }
+        IGenericRepository<UserSkills> UserSkillRepository { get; }
+        void Save();
+        Task SaveAsync();
+    }
+}
